98272540db071cff9a1ce37961d213f86d787740
Takahiro Masui <takahiro.masui@luxiar.com>
2020-06-06 19:02:55 +0900

    use context simply

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,77 @@
-import React from 'react'
+import React, { createContext, useState, useContext, useCallback } from 'react'
 import './App.scss'
 
+type TTitles = {
+  component1: string,
+  component2: string
+}
+
+type TTitlesContext = {
+  titles: TTitles,
+  setTitles: React.Dispatch<React.SetStateAction<TTitles>>
+}
+
+const initialValue = {
+  component1: 'LightGreen Component(use Context)',
+  component2: 'LightBlue Component(use Context)',
+}
+
+const TitlesContext = createContext({} as TTitlesContext)
+
 const App = () => {
+  const [titles, setTitles] = useState(initialValue)
+  console.log('rendering App')
+
   return (
-    <div className="App" >
-      <Component1 />
-      <Component2 />
-      <Component3 />
-    </div>
+    <TitlesContext.Provider value={{ titles, setTitles }}>
+      
+      <div className="App" >
+        <Component1 />
+        <Component2 />
+        <Component3 />
+      </div>
+    </TitlesContext.Provider>
   )
 }
 
 const Component1 = () => {
+  const { titles, setTitles } = useContext(TitlesContext)
+
+  console.log('rendering LightGreen Component')
+
   return (
-    <div className="component1"></div>
+    <>
+      <div className="component1">
+        {titles.component1}
+        <button onClick={() => setTitles({ ...titles, component1: 'checked(use Context)' })}>Checked</button>
+      </div>
+    </>
   )
 }
 
 const Component2 = () => {
+  const { titles, setTitles } = useContext(TitlesContext)
+
+  console.log('rendering LightBlue Component')
   return (
-    <div className="component2"></div>
+    <div className="component2">
+      {titles.component2}
+      <button onClick={() => setTitles({ ...titles, component2: 'checked(use Context)' })}>Checked</button>
+    </div>
   )
 }
 
 const Component3 = () => {
+  const [title, setTitle] = useState({ component3: 'LightYellow Component(use LocalState)'})
+
+  const onClick = useCallback(() => setTitle({ component3: 'checked(use LocalState)' }), [setTitle])
+
+  console.log('rendering LightYellow Component')
   return (
-    <div className="component3"></div>
+    <div className="component3">
+      {title.component3}
+      <button onClick={onClick}>Checked</button>
+    </div>
   )
 }
 
